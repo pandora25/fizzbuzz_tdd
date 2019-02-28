5daf8471570054023ab153735d40324cedd0c952
michael yalda <MCYALDA@yahoo.com>
2019-02-27 21:10:48 -0500

    Implemented & Added tests

--- FizzBuzzTdd/FizzBuzz.Core/FizzBuzzService.cs
+++ FizzBuzzTdd/FizzBuzz.Core/FizzBuzzService.cs
@@ -6,7 +6,22 @@ namespace FizzBuzz.Core
     {
         public string SolveFizzBuzz(int input)
         {
-            return string.Empty;
+            if (input % 3 == 0 && input % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (input % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (input % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return input.ToString();
+            }
         }
     }
 }
--- FizzBuzzTdd/FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzzTdd/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -13,5 +13,29 @@ namespace FizzBuzz.Tests
 
             Assert.AreEqual("Fizz", service.SolveFizzBuzz(3));
         }
+
+        [TestMethod]
+        public void Should_Return_Buzz_When_Input_Is_Multiple_Of_5()
+        {
+            var service = new FizzBuzzService();
+
+            Assert.AreEqual("Buzz", service.SolveFizzBuzz(5));
+        }
+
+        [TestMethod]
+        public void Should_Return_FizzBuzz_When_Input_Is_Multiple_Of_Both_3_And_5()
+        {
+            var service = new FizzBuzzService();
+
+            Assert.AreEqual("FizzBuzz", service.SolveFizzBuzz(15));
+        }
+
+        [TestMethod]
+        public void Should_Return_Same_Input_When_Input_Is_Not_A_Multiple_Of_Either_3_Or_5()
+        {
+            var service = new FizzBuzzService();
+
+            Assert.AreEqual("1", service.SolveFizzBuzz(1));
+        }
     }
 }
