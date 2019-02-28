fe80cdca729c671f9fb340bfdc49c821108f5a77
michael yalda <MCYALDA@yahoo.com>
2019-02-27 21:20:37 -0500

    Re-factored Tests with setup/tear-down per test to minimize duplication

--- FizzBuzzTdd/FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzzTdd/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -6,35 +6,41 @@ namespace FizzBuzz.Tests
     [TestClass]
     public class FizzBuzzTests
     {
+        private FizzBuzzService service;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            service = new FizzBuzzService();
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            service = null;
+        }
+
         [TestMethod]
         public void Should_Return_Fizz_When_Input_Is_Multiple_Of_3()
         {
-            var service = new FizzBuzzService();
-
             Assert.AreEqual("Fizz", service.SolveFizzBuzz(3));
         }
 
         [TestMethod]
         public void Should_Return_Buzz_When_Input_Is_Multiple_Of_5()
         {
-            var service = new FizzBuzzService();
-
             Assert.AreEqual("Buzz", service.SolveFizzBuzz(5));
         }
 
         [TestMethod]
         public void Should_Return_FizzBuzz_When_Input_Is_Multiple_Of_Both_3_And_5()
         {
-            var service = new FizzBuzzService();
-
             Assert.AreEqual("FizzBuzz", service.SolveFizzBuzz(15));
         }
 
         [TestMethod]
         public void Should_Return_Same_Input_When_Input_Is_Not_A_Multiple_Of_Either_3_Or_5()
         {
-            var service = new FizzBuzzService();
-
             Assert.AreEqual("1", service.SolveFizzBuzz(1));
         }
     }
